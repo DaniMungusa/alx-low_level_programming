700f27fc1394521d4a29c55d081c537b12da8838
DaniMungusa <dmungusa.dm@gmail.com>
2023-02-18 10:52:37 -0800

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,18 @@
  * Return: 0
  */
 int main(void)
-{
-	int i = 0;
-
-	for (i = 10; i < 99; i++)
 	{
-		putchar(i);
-		putchar(',');
-	}
+	int i;
 
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
-}
+	}
