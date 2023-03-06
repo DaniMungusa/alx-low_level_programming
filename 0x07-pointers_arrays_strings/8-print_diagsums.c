98157a35703fb049363f47f2aa80a1b449de60b4
DaniMungusa <dmungusa.dm@gmail.com>
2023-03-06 08:35:38 -0800

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * print_diagsums - A function that prints the sum of the two diagonals of a square matrix of integers
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
+ *_memset - fill a block of memory with a specific value
+ *@s: starting address of memory to be filled
+ *@b: the desired value
+ *@n: number of bytes to be changed
+ *
+ *Return: changed array with new value for n bytes
  */
-void print_diagsums(int *a, int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i, n, sum1 = 0, sum2 = 0;
+	int i = 0;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		sum1 = sum1 + a[i];
-
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-	sum2 = sum2 + a[n];
-	printf("%d, %d\n", sum1, sum2);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
