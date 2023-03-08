9bcbf7b8405c9f79eaf7bbad291e0e77d1e05311
DaniMungusa <dmungusa.dm@gmail.com>
2023-03-08 07:16:11 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
  * File: main
- *
- * Desc: header file
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-recursion directory.
  */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
